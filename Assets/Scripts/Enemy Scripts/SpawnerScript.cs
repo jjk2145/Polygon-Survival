5e5bb33bb7db35a2e44e2cee33941053e47881e3
Zach Shuffield <zjs1316@rit.edu>
2016-05-05 23:03:16 -0400

    added a round system with 7 second intervals in between. added a gui text i wanted to enable/disable but it isnt working for some reason.

--- Assets/Scripts/Enemy Scripts/SpawnerScript.cs
+++ Assets/Scripts/Enemy Scripts/SpawnerScript.cs
@@ -6,14 +6,22 @@ public class SpawnerScript : MonoBehaviour {
 	int spawnNumber = 100;
 
 	//Time counter for spawns
-	float spawnTimer = 2;
+	float spawnTimer = 10;
 	//Interval at which to spawn them
 	float spawnCooldown = 2;
+
+	float roundTimeInterval = 7;
+
+	float round = 1;
+
+	bool inbetweenRound = false;
 
 	//Objects to reference
 	public GameObject triangle;
 	public GameObject bomber;
 	public GameObject ScoreKeeper;
+	public GUIText roundText;
+	public GUIText prepareText;
 
 	float HPmultiplyer = 1;
 
@@ -22,33 +30,64 @@ public class SpawnerScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		spawnTimer = Time.time + spawnCooldown;
+		//prepareText = (GUIText)GameObject.Find("prepare text");
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-
-
 		score = ScoreKeeper.GetComponent<ScoreKeeper>().score;
-		if (score >= 500 && score<749) {
+
+		if (score >= 500 && score<749 && round == 1) {
 			spawnCooldown = 1.50f;
+			round++;
+			roundText.text = "Round: " + round;
+			prepareText.enabled = true;
+			spawnTimer += roundTimeInterval;
 		}
-		if (score >= 750 && score<999) {
+		if (score >= 750 && score<999 && round == 2) {
 			spawnCooldown = 1.25f;
+			round++;
+			spawnTimer += roundTimeInterval;
+			roundText.text = "Round: " + round;
+			prepareText.enabled = true;
 		}
-		if (score >= 1000 && score<1999) {
+		if (score >= 1000 && score<1999 && round == 3) {
 			spawnCooldown = 1f;
+			round++;
+			spawnTimer += roundTimeInterval;
+			roundText.text = "Round: " + round;
+			prepareText.enabled = true;
 		}
-		if (score >= 2000 && score<2999) {
+		if (score >= 2000 && score<2999 && round == 4) {
 			spawnCooldown = .75f;
+			round++;
+			spawnTimer += roundTimeInterval;
+			roundText.text = "Round: " + round;
+			prepareText.enabled = true;
 		}
-		/*if (score >= 3000 && score<3999) {
-			spawnCooldown = .50f;
-		}*/
-		if (score >= 4000) {
+		if (score >= 3000 && score<3999 && round == 5) {
+			round++;
+			spawnTimer += roundTimeInterval;
+			roundText.text = "Round: " + round;
+			prepareText.enabled = true;
+		}
+		if (score >= 4000 && round == 6) {
 			//spawnCooldown = .35f;
 			HPmultiplyer = 2f;
+			round++;
+			spawnTimer += roundTimeInterval;
+			roundText.text = "Round: " + round;
+			prepareText.enabled = true;
+		}
+		if (score >= 5000 && round == 7) {
+			//spawnCooldown = .35f;
+			HPmultiplyer = 3f;
+			round++;
+			spawnTimer += roundTimeInterval;
+			roundText.text = "Round: " + round;
+			prepareText.enabled = true;
 		}
 
 
---
+++ Assets/turnoffAfter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turnoffAfter : MonoBehaviour {
+
+	float timeTurnedOn;
+	// Use this for initialization
+	void OnAwake () {
+		timeTurnedOn = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > timeTurnedOn + 6f) {
+			gameObject.SetActive (false);
+		}
+	}
+}
